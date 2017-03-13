87051bddd7a248dd4fc59bfcbf684aac1583e7db
Setyara <solmir.set@gmail.com>
2017-03-13 18:31:03 +0200

    Test

--- test/test.js
+++
@@ -1,6 +0,0 @@
-var myApp = angular.module('myApp', []);
-    myApp.controller('TestCtrl', function ($scope) {
-            $scope.contentSwitch = 'content1';
-            $scope.contentSwitch = 'content2';
-            $scope.data={visible : false};
-        });
